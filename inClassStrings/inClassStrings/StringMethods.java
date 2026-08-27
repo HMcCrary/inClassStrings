92a172ca1de60f24b300498c908893c7a11c4c1a
John Hightower <john.hightower@uafs.edu>
2026-08-27 16:53:41 -0500

    Update

---
+++ inClassStrings/inClassStrings/StringMethods.java
@@ -0,0 +1,30 @@
+package inClassStrings;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		// Test the String Methods
+
+		
+	}
+	
+	public static String toUpperCase(String str) {
+		
+		return null;
+	}
+	
+	public static String subString(String str, int beginIndex, int endIndex) {
+		
+		return null;
+	}
+
+	public static char[] toCharArray(String str) {
+		
+		return null;
+	}
+	
+	public static String[] split(String str,char delim) {
+		
+		return null;
+	}
+}
