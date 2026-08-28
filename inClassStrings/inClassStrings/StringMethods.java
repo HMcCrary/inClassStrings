2298dea0e4dae529391d97dc4f0c75c521c5648a
Hannah McCrary <hmccra100@uafs.edu>
2026-08-28 09:55:02 -0500

    Method toUpperCase().

--- inClassStrings/inClassStrings/StringMethods.java
+++ inClassStrings/inClassStrings/StringMethods.java
@@ -3,14 +3,28 @@ package inClassStrings;
 public class StringMethods {
 
 	public static void main(String[] args) {
-		// Test the String Methods
+		String str = "123abAC/$";
+		
+		System.out.printf("%s\n", toUpperCase(str));
 
 		
 	}
 	
 	public static String toUpperCase(String str) {
+		char letter = ' ';
+		String upper = "";
 		
-		return null;
+		for (int i = 0; i < str.length(); i++) {
+			letter = str.charAt(i);
+			
+			if ((int)letter >= 97 && (int)letter <= 122) {
+				letter = (char)((int)letter - 32);
+			}
+			
+			upper = upper + letter;
+		}
+		
+		return upper;
 	}
 	
 	public static String subString(String str, int beginIndex, int endIndex) {
